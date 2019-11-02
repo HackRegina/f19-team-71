fd01846d84523fc880c113a6ed59755a13b278d6
Mohammad Abu Musa <mmusa@Mohammads-MacBook-Pro.local>
2019-11-01 18:40:34 -0600

    bare bone components baked

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EducationsComponent } from './educations/educations.component';
+import { EducationComponent } from './education/education.component';
+import { EducationDetailComponent } from './education-detail/education-detail.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { QuizResultComponent } from './quiz-result/quiz-result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EducationsComponent
+    EducationsComponent,
+    EducationComponent,
+    EducationDetailComponent,
+    QuizComponent,
+    QuizResultComponent
   ],
   imports: [
     BrowserModule,
