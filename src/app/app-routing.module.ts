9ab2c1e849b52fbd382c66c4f5110e358b617d5d
Mohammad Abu Musa <mmusa@Mohammads-MacBook-Pro.local>
2019-11-01 18:51:57 -0600

    Initial routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,39 @@
+import { QuizResultComponent } from './quiz-result/quiz-result.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { EducationsComponent } from './educations/educations.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EducationComponent } from './education/education.component';
+import { EducationDetailComponent } from './education-detail/education-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'educations'
+  },
+  {
+    path: 'educations',
+    component: EducationsComponent
+  },
+  {
+    path: 'education/:eid',
+    component: EducationComponent
+  },
+  {
+    path: 'education-detail/:eid',
+    component: EducationDetailComponent
+  },
+  {
+    path: 'quiz/:eid/:type',
+    component: QuizComponent
+  },
+  {
+    path: 'quiz-result/:eid/:type',
+    component: QuizResultComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
